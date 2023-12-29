22f61232f71aadd53d70ace8ac5b3e302de30280
Viktor Kryuchkov <crykour12@gmail.com>
2023-12-29 13:41:07 +0300

    feat: connect fonts in tailwind.config.js n index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        BebasNeue: ['"Bebas Neue"', "sans-serif"],
+        AvenirNextCyr: ['"Avenir Next Cyr"', "sans-serif"],
+        Inter: ['Inter', "sans-serif"],
+
+      },
+    },
   },
   plugins: [],
 }
