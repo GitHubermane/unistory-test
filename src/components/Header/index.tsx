fb6a688426259dfe92a5b9fd60d26eaca74d5693
Viktor Kryuchkov <crykour12@gmail.com>
2023-12-29 17:14:21 +0300

    feat: add Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import { Button } from 'components';
+
+export const Header = () => {
+  const isConnected = false;
+  const adress = '0x1HB5XMLmzFVj8ALj6mfBsbifRoD4miY36v';
+
+  return (
+    <div className="flex justify-between items-center h-20 px-16">
+      <div className="flex items-center justify-center h-11 w-48 text-gray-light bg-gray-dark border border-dashed border-gray-light">
+        LOGO
+      </div>
+
+      {isConnected ? (
+        <div className="max-w-36 overflow-hidden text-ellipsis text-orange-light">
+          <span className="text-lg   whitespace-nowrap">{adress}</span>
+        </div>
+      ) : (
+        <Button onClick={() => {}}>Connect metamask</Button>
+      )}
+    </div>
+  );
+};
