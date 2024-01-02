741bc8a65ff19331ce831650bf666202c1e6a55b
Viktor Kryuchkov <crykour12@gmail.com>
2024-01-02 08:51:40 +0300

    feat: add ModalMetamask

--- src/containers/Welcome/ModalMetamask/incex.tsx
+++ src/containers/Welcome/ModalMetamask/incex.tsx
@@ -1,18 +1,16 @@
 import { Button, Modal } from 'components';
 
-export const ModalMetamask = () => {
-  return (
-    <Modal>
-      <div className="flex flex-col items-center">
-        <h3 className="mb-7 text-4xl font-bold text-orange-light">
-          Metamask extension
-        </h3>
-        <p className="w-80 mb-12 font-Inter leading-5">
-          {'To work with our application, you have to install the '}
-          <span className="text-orange-light">Metamask browser extension</span>
-        </p>
-        <Button onClick={() => {}}>Skip this step</Button>
-      </div>
-    </Modal>
-  );
-};
+export const ModalMetamask = () => (
+  <Modal>
+    <div className="flex flex-col items-center">
+      <h3 className="mb-7 text-4xl font-bold text-orange-light">
+        Metamask extension
+      </h3>
+      <p className="w-80 mb-12 font-Inter leading-5">
+        {'To work with our application, you have to install the '}
+        <span className="text-orange-light">Metamask browser extension</span>
+      </p>
+      <Button onClick={() => {}}>Skip this step</Button>
+    </div>
+  </Modal>
+);
