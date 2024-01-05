0074a60eb48dc8c4e8ba749390a6e8f597c6fe6c
Viktor Kryuchkov <crykour12@gmail.com>
2024-01-05 14:35:27 +0300

    feat: add styled scroll to table

--- src/containers/Welcome/RegistrationForm/Table/index.tsx
+++ src/containers/Welcome/RegistrationForm/Table/index.tsx
@@ -30,31 +30,34 @@ export const Table = () => (
       <div className="inline-block w-2/5">Email</div>
       <div className="inline-block w-2/5">Wallet</div>
     </div>
-    <div className="font-AvenirNextCyr">
-      {arr.map(({ name, email, adress }) => (
-        <div
-          className="flex items-center py-4 text-sm text-left border-b last-of-type:border-none"
-          key={email}
-        >
-          <div className="inline-block w-1/5 pr-4 overflow-hidden text-ellipsis">
-            {name}
+    <div className="font-AvenirNextCyr overflow-y-auto h-96 -mr-7 scroll">
+      <div className="mr-7">
+        {arr.map(({ name, email, adress }) => (
+          <div
+            className="flex items-center py-4 text-sm text-left border-b last-of-type:border-none"
+            key={adress}
+          >
+            <div className="inline-block w-1/5 pr-4 overflow-hidden text-ellipsis">
+              {name}
+            </div>
+            <div className="inline-block w-2/5 pr-4 overflow-hidden text-ellipsis">
+              {email}
+            </div>
+            <div className="inline-flex justify-between items-center w-2/5">
+              <span className="w-auto overflow-hidden text-ellipsis pr-4">
+                {adress}
+              </span>
+              <button>
+                <img
+                  className="h-min"
+                  src={CrossIcon}
+                  alt="Cross"
+                />
+              </button>
+            </div>
           </div>
-          <div className="inline-block w-2/5 pr-4 overflow-hidden text-ellipsis">
-            {email}
-          </div>
-          <div className="inline-flex justify-between items-center w-2/5">
-            <span className="w-auto overflow-hidden text-ellipsis pr-4">
-              {adress}
-            </span>
-            <button>
-              <img
-                src={CrossIcon}
-                alt="Cross"
-              />
-            </button>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   </div>
 );
