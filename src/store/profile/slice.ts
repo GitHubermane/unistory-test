f40b427b6e27dd03d50309b36edd242546f49dad
Viktor Kryuchkov <crykour12@gmail.com>
2024-01-06 14:59:03 +0300

    feat: add profile slice

---
+++ src/store/profile/slice.ts
@@ -0,0 +1,25 @@
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+import type { ProfileState } from 'types';
+
+const initialState: Readonly<ProfileState> = {
+  id: null,
+  name: null,
+  email: null,
+  adress: null,
+};
+
+export const profileSlice = createSlice({
+  name: 'profile',
+  initialState,
+  reducers: {
+    setState: (state, action: PayloadAction<Partial<ProfileState>>) => {
+      // eslint-disable-next-line no-param-reassign
+      state = { ...state, ...action };
+    },
+  },
+});
+
+export const { setState } = profileSlice.actions;
+
+export default profileSlice.reducer;
