eba348c4ad5f11ae35d8ed204ea79e36a7ad176b
Viktor Kryuchkov <crykour12@gmail.com>
2024-01-06 18:04:32 +0300

    fix: dapp config

--- src/api/metamask/index.ts
+++ src/api/metamask/index.ts
@@ -4,8 +4,4 @@ import { getDefaultProvider } from 'ethers';
 
 export const config: Config = {
   readOnlyChainId: Mainnet.chainId,
-  readOnlyUrls: {
-    [Mainnet.chainId]: getDefaultProvider('mainnet'),
-    [Goerli.chainId]: getDefaultProvider('goerli'),
-  },
 };
