7fab318cd1b46e3299975cdaf55ca53a3cfb362c
Viktor Kryuchkov <crykour12@gmail.com>
2024-01-07 09:34:12 +0300

    feat: add modal provider

--- src/main.tsx
+++ src/main.tsx
@@ -9,17 +9,22 @@ import { Provider } from 'react-redux';
 import { config } from 'api';
 import { DAppProvider } from '@usedapp/core';
 
+// Modal
+import { ModalProvider } from 'react-modal-hook';
+
 // styles
 import './assets/styles/index.css';
 
 import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <DAppProvider config={config}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </DAppProvider>
-  </Provider>,
+  <ModalProvider>
+    <Provider store={store}>
+      <DAppProvider config={config}>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </DAppProvider>
+    </Provider>
+  </ModalProvider>,
 );
