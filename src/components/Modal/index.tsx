b92f4418d6e21708d95a58233f694c52b1a75051
Viktor Kryuchkov <crykour12@gmail.com>
2024-01-07 09:29:48 +0300

    fix: add position fixed to modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -5,7 +5,7 @@ type PropsType = {
 };
 
 export const Modal: FC<PropsType> = ({ children }) => (
-  <div className="absolute flex items-center justify-center top-0 left-0 h-full w-full bg-slate-900/80 z-20">
+  <div className="fixed flex items-center justify-center top-0 left-0 h-full w-full bg-slate-900/80 z-20">
     <div className="px-12 pt-12 pb-11 bg-black">{children}</div>
   </div>
 );
